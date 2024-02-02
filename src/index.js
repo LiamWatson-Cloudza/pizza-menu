a54f6aa82f3a2721e4bf1d4dd88e854721befd4c
LiamWatson-Clouza <153590157+LiamWatson-Clouza@users.noreply.github.com>
2024-02-02 14:46:02 +0200

    anges to index.js

--- src/index.js
+++ src/index.js
@@ -4,9 +4,15 @@ import ReactDOM from "react-dom/client";
 function App() {
   return <h1>Hello React!</h1>;
 }
+
+//React v18+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
 );
+
+//React v18-
+//React.render(<App />);
+//React.render(<App />, document.getElementById("root"));
