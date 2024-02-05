613385e4c084885f1fccff92ed8e7ae950236807
LiamWatson-Clouza <153590157+LiamWatson-Clouza@users.noreply.github.com>
2024-02-05 13:39:58 +0200

    removed blank line

--- src/index.js
+++ src/index.js
@@ -96,7 +96,6 @@ function Pizza(props) {
       <div>
         <h3>{props.name}</h3>
         <p>{props.ingredients}</p>
-
         <span>{props.price}</span>
         <span>{props.price}</span>
       </div>
