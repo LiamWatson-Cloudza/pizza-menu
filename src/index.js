47c9bb4f0a3cd72151644addc6b2557616053ed4
LiamWatson-Clouza <153590157+LiamWatson-Clouza@users.noreply.github.com>
2024-02-06 10:50:54 +0200

    added title to the index html and added conditional setting of elements

--- src/index.js
+++ src/index.js
@@ -75,17 +75,19 @@ function Menu() {
     <main className="menu">
       <h2>Our Menu</h2>
 
-      <p>
-        Authentic Italian cuizine. 6 creative dishes to choose from. All from
-        our stove oven, all organic, all delicious
-      </p>
-
       {numPizzas > 0 ? (
-        <ul className="pizzas">
-          {pizzas.map((pizza) => (
-            <Pizza pizzaObj={pizza} key={pizza.name} />
-          ))}
-        </ul>
+        //using a react fragment aids the use of more than one element
+        <>
+          <p>
+            Authentic Italian cuizine. 6 creative dishes to choose from. All
+            from our stove oven, all organic, all delicious
+          </p>
+          <ul className="pizzas">
+            {pizzas.map((pizza) => (
+              <Pizza pizzaObj={pizza} key={pizza.name} />
+            ))}
+          </ul>
+        </>
       ) : (
         <p>We're still working on our menu, please come back later:)</p>
       )}
@@ -109,15 +111,23 @@ function Menu() {
 function Pizza({ pizzaObj }) {
   console.log(pizzaObj);
   //removes pizzas that is sold out
-  if (pizzaObj.soldOut) return null;
+  // if (pizzaObj.soldOut) return null;
 
   return (
-    <li className="pizza">
+    //conditionally setting class elements
+    <li className={`pizza ${pizzaObj.soldOut ? "sold-out" : ""}`}>
       <img src={pizzaObj.photoName} alt={pizzaObj.name} />
       <div>
         <h3>{pizzaObj.name}</h3>
         <p>{pizzaObj.ingredients}</p>
-        <span>{pizzaObj.price}</span>
+
+        {/* {pizzaObj.soldOut ? (
+          <span>SOLD OUT</span>
+        ) : (
+          <span>{pizzaObj.price}</span>
+        )} */}
+
+        <span>{pizzaObj.soldOut ? "SOLD OUT" : pizzaObj.price}</span>
       </div>
     </li>
   );
